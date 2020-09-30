60016be9b8c4d1d57fee4396d43bc4e262d3cc17
iamshrikantjha <shrikantjha11@gmail.com>
2020-09-30 19:02:47 +0530

    Updates

---
+++ precache-manifest.75f8b8fc3daa6be963227e534ba34686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088a0291d01bd50113a3d3c61a6022f2",
+    "url": "/tasklist/index.html"
+  },
+  {
+    "revision": "6409b9d371b59b56bba0",
+    "url": "/tasklist/static/css/2.d00bb1db.chunk.css"
+  },
+  {
+    "revision": "be7921908c5579afbbd5",
+    "url": "/tasklist/static/css/main.49a4f4b8.chunk.css"
+  },
+  {
+    "revision": "6409b9d371b59b56bba0",
+    "url": "/tasklist/static/js/2.5aebf774.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/tasklist/static/js/2.5aebf774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7921908c5579afbbd5",
+    "url": "/tasklist/static/js/main.d6f672fa.chunk.js"
+  },
+  {
+    "revision": "27f88e7e759b7e6db014",
+    "url": "/tasklist/static/js/runtime-main.b1e495b3.js"
+  }
+]);
